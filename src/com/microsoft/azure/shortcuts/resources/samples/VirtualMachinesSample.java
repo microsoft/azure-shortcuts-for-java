ac2bd913739c8255ed63c8b5822bd7b553b01909
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2016-01-20 16:42:38 -0800

    bug fix in printVM

--- src/com/microsoft/azure/shortcuts/resources/samples/VirtualMachinesSample.java
+++ src/com/microsoft/azure/shortcuts/resources/samples/VirtualMachinesSample.java
@@ -98,7 +98,7 @@ public class VirtualMachinesSample {
 			.append(String.format("\tComputer name: %s\n", vm.computerName()))
 			.append(String.format("\tCustom data: %s\n", vm.customData()))
 			.append(String.format("\tNumber of data disks: %d\n", vm.dataDisks().size()))
-			.append(String.format("\tNumber of extensions: %d\n", vm.extensions().size()))
+			.append(String.format("\tNumber of extensions: %d\n", (vm.extensions()!=null) ? vm.extensions().size() : 0))
 			.append(String.format("\tGroup: %s\n", vm.group()))
 			;
 			
